7588dd7b61d9f36db7f59a9ef32392d66686776b
lhanyun <2384049697@qq.com>
2024-02-26 21:15:07 +0800

    feat: 测试

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return 'Hello World';
+  }
 
   @Post('screenshot')
   async screenshot(@Body('url') url: string): Promise<string> {
